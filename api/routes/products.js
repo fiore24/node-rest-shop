8202c0ea05c417727dccf3cbf7ce10f00e8baef5
Yulia Liashenko <fiore241987@gmail.com>
2020-01-04 18:49:21 -0800

    Patch,Delete,Get,Post methods was updated to work with MongoDB

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,9 +6,25 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            if (docs.length >= 0){
+                res.status(200).json(docs);
+            }else {
+                res.status(404).json({
+                    message: 'No entries found'
+                });
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -52,14 +68,38 @@ router.get('/:productId',(req, res, next) => {
 
 });
 router.patch('/:productId',(req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Product.update({_id: id }, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch( err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 router.delete('/:productId',(req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
+    const id = req.params.productId;
+    Product.remove({_id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
