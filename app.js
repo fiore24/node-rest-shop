8db9fa59993dfa337c04f02e4d720860461c0e00
Yulia Liashenko <fiore241987@gmail.com>
2020-01-04 18:02:40 -0800

    MongoDB connected with mongoose

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+
+mongoose.connect('mongodb+srv://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-je9aq.mongodb.net/test?retryWrites=true&w=majority',
+    {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -40,10 +46,3 @@ app.use((error, req, res, next) =>{
 
 
 module.exports = app;
-/*
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
-*/
