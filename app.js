b473c2b8931de4b44eb83a56cc289789cbdad442
Yulia Liashenko <fiore241987@gmail.com>
2020-01-09 02:13:03 -0800

    Implemented user signup and email regex validation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 
 mongoose.connect('mongodb+srv://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-je9aq.mongodb.net/test?retryWrites=true&w=majority',
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) =>{
     const error = new Error('Not found');
